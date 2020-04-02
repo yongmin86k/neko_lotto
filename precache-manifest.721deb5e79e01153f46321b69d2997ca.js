e60b0ba84d06b554e5a352a6f2f16ee2a3a11d88
Yongmin Kim <yongmin86k@gmail.com>
2020-04-02 03:49:38 -0700

    Updates

---
+++ precache-manifest.721deb5e79e01153f46321b69d2997ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d9ebb780715348b55ba60055f2e53b",
+    "url": "/neko_lotto/index.html"
+  },
+  {
+    "revision": "e5819ab19304d73939c2",
+    "url": "/neko_lotto/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "e663806e190399f1ba7f",
+    "url": "/neko_lotto/static/css/main.5e45bb0f.chunk.css"
+  },
+  {
+    "revision": "e5819ab19304d73939c2",
+    "url": "/neko_lotto/static/js/2.44a130e3.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/neko_lotto/static/js/2.44a130e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e663806e190399f1ba7f",
+    "url": "/neko_lotto/static/js/main.4a2dca7d.chunk.js"
+  },
+  {
+    "revision": "d399d9bd7a7f1c2744b7",
+    "url": "/neko_lotto/static/js/runtime-main.35bc5976.js"
+  }
+]);
